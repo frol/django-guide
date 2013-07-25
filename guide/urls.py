cb60d6eab67a7543c906c5ea64e16a610a8d500f
frol <frolvlad@gmail.com>
2013-07-24 21:13:39 -0700

    add Django 1.6 support

--- guide/urls.py
+++ guide/urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls.defaults import patterns, url
+try:
+    from django.conf.urls import patterns, url
+except ImportError:
+    from django.conf.urls.defaults import patterns, url # Django < 1.4
 
 urlpatterns = patterns('guide.views',
     url('^toggle/(?P<status>enable|disable)/$', 'toggle_guides', name='toggle_guides'),
